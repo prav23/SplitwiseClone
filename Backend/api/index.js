78ce226b198768e6ca8badb02236426b3b1d9256
Praveen Anguru <praveenkumar.anguru@sjsu.edu>
2021-03-05 14:52:58 -0800

    removing test code files

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const test = require('./test');
 const login = require('./login');
 const register = require('./register');
 const router = express.Router();
 
-router.get('/test', test);
 router.post('/login', login);
 router.post('/register', register);
 //router.get('/login', login);
--- Backend/api/test.js
+++
@@ -1,5 +0,0 @@
-const test = async(req, res) => {
-    res.end("Test");
-}
-
-module.exports = test;
